3ec7fa3de680080bac691ea48a44d46a7378fe4c
hwb <luckhwb@163.com>
2018-06-29 13:59:34 +0800

    add .

---
+++ client2/src/main/java/com/example/demo/Demo.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Hua wb on 2018/5/11.
+ */
+@EnableEurekaClient
+@RestController
+public class Demo {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
---
+++ service-ribbon/src/main/java/com/example/demo/HelloControler.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Hua wb on 2018/5/12.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+}
