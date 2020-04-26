f81f2abff393e2736731e02c6f3bc903bd71328b
hwb <luckhwb@163.com>
2020-04-26 10:59:07 +0800

    zuul 不可用

---
+++ client-zuul/src/main/java/com/luck/cloud/zuul/ClientZuulApplication.java
@@ -0,0 +1,28 @@
+package com.luck.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Enablecl
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ClientZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientZuulApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+        // 开启负载均衡
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
