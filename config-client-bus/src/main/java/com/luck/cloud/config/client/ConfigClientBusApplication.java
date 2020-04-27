8101fdd98b0d08ad54f114edd43d39fc4746456c
hwb <luckhwb@163.com>
2020-04-27 18:33:30 +0800

    config bus

---
+++ config-client-bus/src/main/java/com/luck/cloud/config/client/ConfigClientBusApplication.java
@@ -0,0 +1,13 @@
+package com.luck.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientBusApplication.class, args);
+    }
+
+}
