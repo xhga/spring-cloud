068c3402f1adba24fb0952ba23979254e6d5cd29
hwb <luckhwb@163.com>
2020-09-22 10:39:41 +0800

    add swagger

---
+++ client/src/main/java/com/example/demo/SwaggerController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Hua wb on 2018/5/11.
+ */
+@EnableEurekaClient
+@RestController
+@Api(tags = "test")
+public class SwaggerController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "hi,i am from port:" + port;
+    }
+
+    @ApiOperation(value = "获取otp", notes="通过手机号获取OTP验证码")
+    @ApiImplicitParam(name = "telephone", value = "电话号码", paramType = "query", required = true, dataType = "Integer")
+    @RequestMapping(value = "getotp", method= RequestMethod.GET)
+    @ResponseBody
+    public String getOtp(@RequestParam(name = "telephone", required = false) String telphone) {
+        return "hello world";
+    }
+}
